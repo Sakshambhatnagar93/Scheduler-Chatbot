47e4dc4dd3e7bbbec41992e4bb79e576ba1cb9d2
Saksham Bhatnagar <137995825+Sakshambhatnagar93@users.noreply.github.com>
2025-03-29 21:06:55 +0530

    Update main.py
    Added sample inputs and responses

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import joblib
+import pandas as pd
 
 # Load trained model and vectorizer
 model = joblib.load("conversation_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
+df = pd.read_csv("merged_conversation_dataset.csv")
 
 # Streamlit UI
-st.title("AI Chatbot")
+st.title("Scheduler Chatbot")
 
 # Function to get response
 def get_response(user_input):
     X_input = vectorizer.transform([user_input])
     return model.predict(X_input)[0]
+
+#sample inputs and responses
+st.subheader("Sample Inputs and Responses:- ")
+st.write(df.head(20))
 
 # User input
 user_input = st.text_input("Enter your message:")
